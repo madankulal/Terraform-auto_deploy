7291b8461ead3ed3139ad3c6695b3ed0a01fa3ce
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-03-26 17:51:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "azurerm" {
 
 # Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "madankumar-resource-group"
+  name     = "madan-resource-group"
   location = "East US"
 }
