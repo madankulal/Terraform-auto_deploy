183a4c2e36e56093efc7187afb72c61df9328723
MADANKUMAR S <46098951+madankulal@users.noreply.github.com>
2026-03-27 09:49:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,24 @@ resource "azurerm_resource_group" "rg" {
   name     = "madan-resource-group"
   location = "East US"
 }
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksdns"
+
+  default_node_pool {
+    name       = "nodepool1"
+    node_count = var.node_count
+    vm_size    = var.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
